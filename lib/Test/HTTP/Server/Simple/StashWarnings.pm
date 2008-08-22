23aae69099cb4bf4090ba64739995df654e20801
Shawn Moore <sartak@bestpractical.com>
2008-08-22 12:53:54 +0000

    import (from Test-HTTP-Server-Simple)

---
+++ lib/Test/HTTP/Server/Simple/StashWarnings.pm
@@ -0,0 +1,73 @@
+#!/usr/bin/env perl
+package Test::HTTP::Server::Simple::StashWarnings;
+use strict;
+use warnings;
+use base 'Test::HTTP::Server::Simple';
+
+use NEXT;
+use Storable ();
+
+sub test_warning_path {
+    my $self = shift;
+    die "You must override test_warning_path in $self to tell " . __PACKAGE__ . " where to provide test warnings.";
+}
+
+sub background {
+    my $self = shift;
+
+    local $SIG{__WARN__} = sub {
+        push @{ $self->{'thss_stashed_warnings'} }, @_;
+        warn @_ if $ENV{TEST_VERBOSE};
+    };
+
+    return $self->NEXT::background(@_);
+}
+
+sub handler {
+    my $self = shift;
+
+    if ($self->{thss_test_path_hit}) {
+        my @warnings = splice @{ $self->{'thss_stashed_warnings'} };
+        my $content  = Storable::nfreeze(\@warnings);
+
+        print "HTTP/1.0 200 OK\r\n";
+        print "Content-Type: application/x-perl\r\n";
+        print "Content-Length: ", length($content), "\r\n";
+        print "\r\n";
+        print $content;
+
+        return;
+    }
+
+    return $self->NEXT::handler(@_);
+}
+
+sub setup {
+    my $self = shift;
+    my @copy = @_;
+
+    while (my ($item, $value) = splice @copy, 0, 2) {
+        if ($item eq 'request_uri') {
+            $self->{thss_test_path_hit} = $value eq $self->test_warning_path;
+        }
+    }
+
+    return $self->NEXT::setup(@_);
+}
+
+sub decode_warnings {
+    my $self = shift;
+    my $text = shift;
+
+    return @{ Storable::thaw($text) };
+}
+
+sub DESTROY {
+    my $self = shift;
+    for (@{ $self->{'thss_stashed_warnings'} }) {
+        warn "Unhandled warning: $_";
+    }
+}
+
+1;
+
